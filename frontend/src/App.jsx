2be882e4637571df05e979f8a1196c5aa08c9db0
Abhirup Das <147976109+abhirup0421L@users.noreply.github.com>
2026-05-29 13:23:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -267,7 +267,7 @@ function App() {
   };
 
   const logout = async () => {
-    await api.post(`/logout/${currentUser}?manual=true`);
+    await api.post(`/logout/${currentUser}`);
 
     localStorage.removeItem("user_id");
     setShowSelfProfile(false);
